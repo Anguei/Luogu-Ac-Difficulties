95b5d412d4510cecd28d79abd9330e9f41f10d57
Anguei <42708316+Anguei@users.noreply.github.com>
2018-08-29 22:52:55 +0800

    转为空格缩进

--- main3.py
+++ main3.py
@@ -13,11 +13,11 @@ user = input()
 urlUser = 'https://www.luogu.org/space/show?uid=' + user
 urlBase = 'https://www.luogu.org/problemnew/show/'
 headers = {
-	u'Referer': u'https://www.luogu.org/recordnew/lists?uid=10000',
-	u'Accept': u'text/html, */*; q=0.01',
-	u'Accept-Encoding': u'gzip, deflate, br',
-	u'Accept-Language': u'zh-CN,zh;q=0.9',
-	u'User-Agent': u'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36'
+    u'Referer': u'https://www.luogu.org/recordnew/lists?uid=10000',
+    u'Accept': u'text/html, */*; q=0.01',
+    u'Accept-Encoding': u'gzip, deflate, br',
+    u'Accept-Language': u'zh-CN,zh;q=0.9',
+    u'User-Agent': u'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36'
 }
 
 # 搞搞 UTF-8
@@ -33,7 +33,7 @@ soup1 = BeautifulSoup(v.text, 'lxml')
 res = soup1.find('div', {'class': 'lg-article am-hide-sm'})
 topics = res.text.split()
 for i in range(len(topics)):
-	topics[i] = topics[i][1 : -1]
+    topics[i] = topics[i][1 : -1]
 
 # 第一个元素不是题目
 topics.pop(0)
@@ -41,48 +41,48 @@ topics.pop(0)
 finished = 0
 
 for topic in topics:
-	try:
-		v = requests.get(url = urlBase + topic, headers = headers, verify = False, timeout = 10)
-	except:
-		#v = requests.get(url = urlBase + topic, headers = headers, verify = False, timeout = 10)
-		#v = requests.get(url = urlBase + topic, headers = headers, verify = False, timeout = 10)
-		print(topic, "Error!")
-		continue
-	
-	#f.write(v.text)
-	v = v.text#.decode('utf-8').encode(type)
-	#print(v)
+    try:
+        v = requests.get(url = urlBase + topic, headers = headers, verify = False, timeout = 10)
+    except:
+        #v = requests.get(url = urlBase + topic, headers = headers, verify = False, timeout = 10)
+        #v = requests.get(url = urlBase + topic, headers = headers, verify = False, timeout = 10)
+        print(topic, "Error!")
+        continue
+    
+    #f.write(v.text)
+    v = v.text#.decode('utf-8').encode(type)
+    #print(v)
 
-	soup = BeautifulSoup(v, 'lxml')
-	try:
-		difficulty = soup.findAll('span', {'class': 'lg-right'})[2].text # 获取难度
-		difficulties.append(difficulty)
-		# 不知道为什么，每次尝试用字典都爆异常，所以暴力 if 
-		if difficulty == '普及/提高-':
-			print(topic, "Yellow")
-			yellow.append(topic)
-		elif difficulty == '普及+/提高':
-			print(topic, "Green!")
-			green.append(topic)
-		elif difficulty == '提高+/省选-':
-			print(topic, "Blue!!")
-			blue.append(topic)
-		elif difficulty == '省选/NOI-':
-			print(topic, "Purple!!!")
-			purple.append(topic)
-		elif difficulty == 'NOI/NOI+/CTSC':
-			print(topic, "BlueDark!!!!")
-			bluedark.append(topic)
-		elif difficulty == '尚无评定':
-			print(topic, "Gray??")
-			gray.append(topic)
-		else:
-			print(topic)
-	except:
-		print(topic, "Didn't allowed!") # 无权查看的题目
-	
-	finished += 1
-	os.system('title U' + user + ': ' + str(finished) + '/' + str(len(topics)))
+    soup = BeautifulSoup(v, 'lxml')
+    try:
+        difficulty = soup.findAll('span', {'class': 'lg-right'})[2].text # 获取难度
+        difficulties.append(difficulty)
+        # 不知道为什么，每次尝试用字典都爆异常，所以暴力 if 
+        if difficulty == '普及/提高-':
+            print(topic, "Yellow")
+            yellow.append(topic)
+        elif difficulty == '普及+/提高':
+            print(topic, "Green!")
+            green.append(topic)
+        elif difficulty == '提高+/省选-':
+            print(topic, "Blue!!")
+            blue.append(topic)
+        elif difficulty == '省选/NOI-':
+            print(topic, "Purple!!!")
+            purple.append(topic)
+        elif difficulty == 'NOI/NOI+/CTSC':
+            print(topic, "BlueDark!!!!")
+            bluedark.append(topic)
+        elif difficulty == '尚无评定':
+            print(topic, "Gray??")
+            gray.append(topic)
+        else:
+            print(topic)
+    except:
+        print(topic, "Didn't allowed!") # 无权查看的题目
+    
+    finished += 1
+    os.system('title U' + user + ': ' + str(finished) + '/' + str(len(topics)))
 
 print('------------------------------------')
 
@@ -90,32 +90,32 @@ yellowPlus, greenPlus = 0, 0
 
 difficulties.sort()
 for i in difficulties:
-	#print i#, d[i]
-	if i == '入门难度':
-		colors['1r'] += 1
-	if i == '普及-':
-		colors['2o'] += 1
-	if i == '普及/提高-':
-		colors['3y'] += 1
-		yellowPlus += 1
-	if i == '普及+/提高':
-		colors['4g'] += 1
-		yellowPlus += 1
-		greenPlus += 1
-	if i == '提高+/省选-':
-		colors['5b'] += 1
-		yellowPlus += 1
-		greenPlus += 1
-	if i == '省选/NOI-':
-		colors['6p'] += 1
-		yellowPlus += 1
-		greenPlus += 1
-	if i == 'NOI/NOI+/CTSC':
-		colors['7d'] += 1
-		yellowPlus += 1
-		greenPlus += 1
-	if i == '尚无评定':
-		colors['8g'] += 1
+    #print i#, d[i]
+    if i == '入门难度':
+        colors['1r'] += 1
+    if i == '普及-':
+        colors['2o'] += 1
+    if i == '普及/提高-':
+        colors['3y'] += 1
+        yellowPlus += 1
+    if i == '普及+/提高':
+        colors['4g'] += 1
+        yellowPlus += 1
+        greenPlus += 1
+    if i == '提高+/省选-':
+        colors['5b'] += 1
+        yellowPlus += 1
+        greenPlus += 1
+    if i == '省选/NOI-':
+        colors['6p'] += 1
+        yellowPlus += 1
+        greenPlus += 1
+    if i == 'NOI/NOI+/CTSC':
+        colors['7d'] += 1
+        yellowPlus += 1
+        greenPlus += 1
+    if i == '尚无评定':
+        colors['8g'] += 1
 print('------------------------------------')
 
 print('Red:', colors['1r'])
@@ -131,101 +131,25 @@ print('------------------------------------')
 
 print('\n' + str(colors['3y']), 'Yellow:')
 for i in range(len(yellow)):
-	print(yellow[i], end = ' \n'[(i + 1) % 10 == 0])
+    print(yellow[i], end = ' \n'[(i + 1) % 10 == 0])
 print('\n' + str(colors['4g']), 'Green:')
 for i in range(len(green)):
-	print(green[i], end = ' \n'[(i + 1) % 10 == 0])
+    print(green[i], end = ' \n'[(i + 1) % 10 == 0])
 print('\n' + str(colors['5b']), 'Blue:')
 for i in range(len(blue)):
-	print(blue[i], end = ' \n'[(i + 1) % 10 == 0])
+    print(blue[i], end = ' \n'[(i + 1) % 10 == 0])
 print('\n' + str(colors['6p']), 'Purple:')
 for i in range(len(purple)):
-	print(purple[i], end = ' \n'[(i + 1) % 10 == 0])
+    print(purple[i], end = ' \n'[(i + 1) % 10 == 0])
 print('\n' + str(colors['7d']), 'Bluedark:')
 for i in range(len(bluedark)):
-	print(bluedark[i], end = ' \n'[(i + 1) % 10 == 0])
+    print(bluedark[i], end = ' \n'[(i + 1) % 10 == 0])
 print('\n' + str(colors['8g']), 'Gray:')
 for i in range(len(gray)):
-	print(gray[i], end = ' \n'[(i + 1) % 10 == 0])
+    print(gray[i], end = ' \n'[(i + 1) % 10 == 0])
 print("\n")
 
 print('------------------------------------')
 
 print('Yellow+: ', yellowPlus)
 print('Green+: ', greenPlus)
-
-
-
-'''
-#include <cstdio>
-#include <cctype>
-#include <cstring>
-#include <string>
-#include <iostream>
-
-int cnt = 0;
-
-std::string subPrefix(std::string s, int len)
-{
-    std::string res = "";
-    for (int i = 0; i < len; ++i)   
-        res += std::string(1, s[i]);
-    return res;
-}
-
-bool isNum(std::string s)
-{
-    for (auto i : s)
-        if (!isdigit(i))
-            return false;
-    return true;
-}
-
-void printColor(const std::string &s, const std::string &color, std::string &rgb)
-{
-	if (color == "Yellow:")
-		rgb = "f1c40f";
-	if (color == "Green:")
-		rgb = "5eb95e";
-	if (color == "Blue:")
-		rgb = "3498db";
-	if (color == "Purple:")
-		rgb = "9b59b6";
-	if (color == "Bluedark:")
-		rgb = "34495e";
-	if (color == "Gray:")
-		rgb = "bbbbbb";
-	printf("\n\n![%s](https://img.shields.io/badge/%s-%s-%s.svg)\n\n> ", s.c_str(), s.c_str(), color.substr(0, color.size() - 1).c_str(), rgb.c_str());
-	cnt = 0;
-	printf("|  |  |  |  |  |  |  |  |  |  |\n| :----------- | :----------- | :----------- | :----------- | :----------- | :----------- | :----------- | :----------- | :----------- | :----------- |\n| ");
-}
-
-int main()
-{
-#ifdef yyfLocal 
-	freopen("data.txt", "r", stdin);
-	freopen("result.txt", "w", stdout);
-#endif
-    std::string s, rgb, color;
-    while (std::cin >> s)
-    {
-        if (isNum(s))
-        {
-            std::cin >> color;
-            printColor(s, color, rgb);
-            continue;
-        }
-        int preLen = 1;
-        if (s[0] == 'U')
-            preLen = 3;
-        if (s[0] == 'C' || s[0] == 'S' || s[0] == 'A')
-            preLen = 2;
-        const char *prefix = subPrefix(s, preLen).c_str();
-        const char *suffix = s.substr(preLen).c_str();
-		printf("[![%s](https://img.shields.io/badge/%s-%s-%s.svg)](https://www.luogu.org/problemnew/show/%s) $\\ $|", s.c_str(), prefix, suffix, rgb.c_str(), s.c_str());
-		++cnt;
-		if (cnt % 8 == 0) // 一行最多几个 badge
-			printf("\n| ");
-    }
-}
-'''
